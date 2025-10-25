785c32c16480a6204ca520e6e6583bcaa4b7c57e
nandosabino <sabinofernando05@gmail.com>
2025-10-25 18:12:33 -0300

    alterações no back-end

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,22 @@
 export default {
   dialect: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "12345",
-  database: "usersdb",
+  host: process.env.DB_HOST || "localhost",
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASS || "12345",
+  database: process.env.DB_NAME || "usersdb",
+  port: process.env.DB_PORT || 5432,
   define: {
     timestamp: true,
     underscored: true,
     underscoredAll: true,
   },
+  dialectOptions: process.env.DATABASE_URL
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
+  url: process.env.DATABASE_URL || null,
 };
