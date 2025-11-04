0ab3e371957a17d4c12cb0b8a4987e0296805a62
nandosabino <sabinofernando05@gmail.com>
2025-11-04 10:30:04 -0300

    Railway deployment

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const isProduction = !!process.env.DATABASE_URL;
+
 export default {
   dialect: "postgres",
-  host: process.env.DB_HOST || "localhost",
-  username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASS || "12345",
-  database: process.env.DB_NAME || "usersdb",
-  port: process.env.DB_PORT || 5432,
+  ...(isProduction
+    ? {
+        url: process.env.DATABASE_URL,
+        dialectOptions: {},
+      }
+    : {
+        host: process.env.DB_HOST || "localhost",
+        username: process.env.DB_USER || "postgres",
+        password: process.env.DB_PASS || "12345",
+        database: process.env.DB_NAME || "usersdb",
+        port: process.env.DB_PORT || 5432,
+      }),
   define: {
     timestamp: true,
     underscored: true,
     underscoredAll: true,
   },
-  dialectOptions: process.env.DATABASE_URL
-    ? {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      }
-    : {},
-  url: process.env.DATABASE_URL || null,
 };
